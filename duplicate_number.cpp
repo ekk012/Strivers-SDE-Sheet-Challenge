f50ef8cfd4fd4a21b3728b2fd66f0de1e403b7e7
Aryan Singh <aryansingh729@gmail.com>
2023-06-13 09:16:07 +0530

    find duplicate number

---
+++ duplicate_number.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include<array>
+#include<iostream>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        map<int,int> m;
+        int count;
+        for(int i=0 ; i < nums.size(); i++){
+            m[nums[i]++]++;
+        }
+        for(auto j: m){
+            if(j.second > 1)
+            count = j.first;
+        }
+        return count;
+    }
+};
