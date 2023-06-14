f941ef10201195e9696e3690c093f67f02f6c0ad
Aryan Singh <aryansingh729@gmail.com>
2023-06-14 10:43:21 +0530

    reverse a linked list

---
+++ reverse_linked_list.cpp
@@ -0,0 +1,35 @@
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include<array>
+#include<iostream>
+
+using namespace std;
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* current = head;
+        ListNode* prev = NULL;
+        while (current) {
+            ListNode* nxt ;
+            nxt = current->next;
+            current->next = prev;
+            prev = current;
+            current = nxt;
+       
+        }
+         return prev;
+    }
+};
