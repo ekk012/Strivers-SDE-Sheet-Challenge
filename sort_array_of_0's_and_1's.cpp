3907bb55506addbb2c059a3ce1779881fbe025d8
Aryan Singh <aryansingh729@gmail.com>
2023-06-29 08:37:57 +0530

    sort array of 0s and 1s

---
+++ sort_array_of_0's_and_1's.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include<array>
+#include<iostream>
+#include<stack>
+using namespace std;
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+    int low = 0, mid = 0, high = nums.size() - 1;
+
+    while (mid <= high) {
+        if (nums[mid] == 0) {
+            swap(nums[low], nums[mid]);
+            low++;
+            mid++;
+        }
+        else if (nums[mid] == 1) {
+            mid++;
+        }
+        else {
+            swap(nums[mid], nums[high]);
+            high--;
+        }
+    }
+    }
+};
