9a14fa618b1be72e9ff98494a36a3eda482733ca
Aryan Singh <aryansingh729@gmail.com>
2023-07-04 09:25:00 +0530

    single element in sorted array

---
+++ single_element_in_sorted_array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include<array>
+#include<iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+           int n = nums.size();
+            int elem = 0;
+            for (int i = 0; i < n; i++) {
+                elem = elem ^ nums[i];
+            }
+            
+            return elem;
+    }
+};
