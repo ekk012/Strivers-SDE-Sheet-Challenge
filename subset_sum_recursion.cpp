e672a8ed3056e9801232a63b36d1027abe07bfc3
Aryan Singh <aryansingh729@gmail.com>
2023-07-07 18:07:17 +0530

    subset sum

---
+++ subset_sum_recursion.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include<array>
+#include<iostream>
+#include<set>
+#include<algorithm>
+using namespace std;
+class Solution {
+  public:
+    void subsetSumsHelper(int ind, vector < int > & arr, int n, vector < int > & ans, int sum) {
+      if (ind == n) {
+        ans.push_back(sum);
+        return;
+      }
+      //element is picked
+      subsetSumsHelper(ind + 1, arr, n, ans, sum + arr[ind]);
+      //element is not picked
+      subsetSumsHelper(ind + 1, arr, n, ans, sum);
+    }
+  vector < int > subsetSums(vector < int > arr, int n) {
+    vector < int > ans;
+    subsetSumsHelper(0, arr, n, ans, 0);
+    sort(ans.begin(), ans.end());
+    return ans;
+  }
+};
